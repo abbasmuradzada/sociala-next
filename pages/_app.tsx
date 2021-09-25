cfed8cefdc2b861786f3e62cfffa1a356d91c9de
elvinhuseynov <huseynovelvin2018@gmail.com>
2021-09-25 12:07:46 +0400

    ant design, sass, react hook form added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import "../styles/globals.css";
-
 import { Provider } from "react-redux";
 import type { AppProps } from "next/app";
-
+import "../styles/globals.css";
+import "../styles/test.scss";
 import store from "../app/store";
 
 const App = ({ Component, pageProps }: AppProps) => (
