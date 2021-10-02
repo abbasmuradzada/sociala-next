981cea2e4da2d8a30c0326ac6d73c6d2b1f8c013
Abbas Muradzada <abbas.muradzada@cybetnet.az>
2021-10-02 12:15:55 +0400

    login page added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
-import { Provider } from "react-redux";
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
-import "../styles/test.scss";
+import { Provider } from "react-redux";
 import store from "../app/store";
+import "../styles/global.scss";
+import "../styles/globals.css";
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <Provider store={store}>
-    <Component {...pageProps} />
-  </Provider>
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  if (Component?.getLayout) {
+    return Component?.getLayout(<Component {...pageProps} />);
+  }
+
+  return (
+    <Provider store={store}>
+      <h1>Header</h1>
+      <Component {...pageProps} />
+      <h1>Footer</h1>
+    </Provider>
+  );
+};
 
 export default App;
